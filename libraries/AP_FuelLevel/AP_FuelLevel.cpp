b6dc1ada449d675952f6e2b8c1add7b076fa511e
AnthonySabbagh <ajrsabbagh@gmail.com>
2022-09-01 09:54:19 -0400

    Removed more unused code

--- libraries/AP_FuelLevel/AP_FuelLevel.cpp
+++ libraries/AP_FuelLevel/AP_FuelLevel.cpp
@@ -1,21 +1,4 @@
 #include <AP_FuelLevel/AP_FuelLevel.h>
-/*const AP_Param::GroupInfo AP_FuelLevel::var_info[] = {
-    // @Param: VOLTAGE_OFFSET
-    // @DisplayName: Fuel sensor voltage offset
-    // @Description: Calibrates fuel sensor
-    // @Range: -5 5
-    // @User: Advanced
-    AP_GROUPINFO("VOLTAGE_OFFSET", 0, AP_FuelLevel, _offset, 0),
-
-    // @Param: VOLTAGE_SCALAR
-    // @DisplayName: Fuel sensor voltage scalar
-    // @Description: Calibrates fuel sensor
-    // @Range: -32768 32768
-    // @User: Advanced
-    AP_GROUPINFO("VOLTAGE_SCALAR", 1, AP_FuelLevel, _scalar, 1),
-
-    AP_GROUPEND
-};*/
 
 extern const AP_HAL::HAL& hal;
 
