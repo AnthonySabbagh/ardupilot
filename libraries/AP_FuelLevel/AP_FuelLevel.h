8840211267aab66adc1ac5882db2f240d3d3ae16
AnthonySabbagh <ajrsabbagh@gmail.com>
2022-09-01 09:53:55 -0400

    Removed commented code

--- libraries/AP_FuelLevel/AP_FuelLevel.h
+++ libraries/AP_FuelLevel/AP_FuelLevel.h
@@ -1,15 +1,11 @@
 #pragma once
 #include <AP_HAL/AP_HAL.h>
-//#include <AP_Param/AP_Param.h>
 
 class AP_FuelLevel{
 public:
     AP_FuelLevel();
     AP_FuelLevel(int16_t pin);
     float get_level();
-//    static const struct AP_Param::GroupInfo var_info[];
 private:
     AP_HAL::AnalogSource *source;
- //   AP_Float _offset;
-   // AP_Float _scalar;
 };
